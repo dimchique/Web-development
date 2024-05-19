8030c45f1451e82e492325627bb3dc09673d09d8
dimchique <dmitrinolimov@gmail.com>
2024-05-20 01:58:44 +1000

    lil changes

--- second_semester/lab4/program.js
+++ second_semester/lab4/program.js
@@ -5,7 +5,17 @@ let svg = d3.select("svg").attr("width", width).attr("height", height);
 cx = ((1 + Math.cos(3 * Math.PI / 2)) * Math.cos(3 * Math.PI / 2)) * width / 5;
 cy = ((1 + Math.cos(3 * Math.PI / 2)) * Math.sin(3 * Math.PI / 2)) * height / 5 + 120;
 
-let pict = svg.append("circle").attr("cx", width / 2).attr("cy", height / 3.333).attr("r", 20).attr("fill", "red");
+let pict = svg.append("g");
+pict.append("circle").attr("cx", width / 2).attr("cy", height / 3.333).attr("r", 40).attr("fill", "red");
+pict.append("circle").attr("cx", width / 2 - 20).attr("cy", height / 3.333 - 10).attr("r", 5).style("fill", "yellow");
+pict.append("circle").attr("cx", width / 2 - 20).attr("cy", height / 3.333 - 10).attr("r", 1).style("fill", "black");
+pict.append("circle").attr("cx", width / 2 + 20).attr("cy", height / 3.333 - 10).attr("r", 5).style("fill", "yellow");
+pict.append("circle").attr("cx", width / 2 + 20).attr("cy", height / 3.333 - 10).attr("r", 1).style("fill", "black");
+pict.append("ellipse").attr("cx", width / 2).attr("cy", height / 3.333 + 20).attr("rx", 30 / 2).attr("ry", 10 / 2).attr("fill", "black");
+pict.append("line").attr("x1", width / 2 - 20).attr("y1", height / 3.333 - 30).attr("x2", width / 2 - 10).attr("y2", height / 3.333 - 40).attr("stroke", "black").attr("stroke-width", 2);
+pict.append("line").attr("x1", width / 2 - 10).attr("y1", height / 3.333 - 30).attr("x2", width / 2).attr("y2", height / 3.333 - 40).attr("stroke", "black").attr("stroke-width", 2);
+pict.append("line").attr("x1", width / 2).attr("y1", height / 3.333 - 30).attr("x2", width / 2 + 10).attr("y2", height / 3.333 - 40).attr("stroke", "black").attr("stroke-width", 2);
+pict.append("line").attr("x1", width / 2 + 10).attr("y1", height / 3.333 - 30).attr("x2", width / 2 + 20).attr("y2", height / 3.333 - 40).attr("stroke", "black").attr("stroke-width", 2);
 
 function createPath() {
     let data = [];
@@ -39,13 +49,23 @@ let dataForm = document.getElementById("setting");
 let runAnimation = () => {
     let animSpeed = dataForm.animSpeed.value;
     let path = drawPath();
-    let pict = svg.append("circle").attr("cx", cx).attr("cy", cy).attr("r", 20).attr("fill", "red");
+    let pict = svg.append("g");
+    pict.append("circle").attr("cx", cx).attr("cy", cy).attr("r", 40).attr("fill", "red");
+    pict.append("circle").attr("cx", cx - 20).attr("cy", cy - 10).attr("r", 5).style("fill", "yellow");
+    pict.append("circle").attr("cx", cx - 20).attr("cy", cy - 10).attr("r", 1).style("fill", "black");
+    pict.append("circle").attr("cx", cx + 20).attr("cy", cy - 10).attr("r", 5).style("fill", "yellow");
+    pict.append("circle").attr("cx", cx + 20).attr("cy", cy - 10).attr("r", 1).style("fill", "black");
+    pict.append("ellipse").attr("cx", cx).attr("cy", cy + 20).attr("rx", 30 / 2).attr("ry", 10 / 2).attr("fill", "black");
+    pict.append("line").attr("x1", cx - 20).attr("y1", cy - 30).attr("x2", cx - 10).attr("y2", cy - 40).attr("stroke", "black").attr("stroke-width", 2);
+    pict.append("line").attr("x1", cx - 10).attr("y1", cy - 30).attr("x2", cx).attr("y2", cy - 40).attr("stroke", "black").attr("stroke-width", 2);
+    pict.append("line").attr("x1", cx).attr("y1", cy - 30).attr("x2", cx + 10).attr("y2", cy - 40).attr("stroke", "black").attr("stroke-width", 2);
+    pict.append("line").attr("x1", cx + 10).attr("y1", cy - 30).attr("x2", cx + 20).attr("y2", cy - 40).attr("stroke", "black").attr("stroke-width", 2);
     const length = path.node().getTotalLength();
     pict.transition().ease(d3.easeLinear).duration(animSpeed * 1000).attrTween('transform', function () {
         return function (t) {
             let { x, y } = path.node().getPointAtLength(t * length);
             let scaleX = 1 + t * (dataForm.sxTo.value - 1);
-            let scaleY = 1 + t * (dataForm.syTo.value - 1); 
+            let scaleY = 1 + t * (dataForm.syTo.value - 1);
             let translate = `translate(${x},${y})`;
             let transform = `scale(${scaleX}, ${scaleY})`;
             return `${translate} ${transform}`;
